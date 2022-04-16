509496d54df0c510936de47f2d1ab3fdfdf1acc0
NickFarEast <shramko.dt@gmail.com>
2022-04-16 19:49:43 +1000

    БД заполнена данными

--- load_fixtures.py
+++ load_fixtures.py
@@ -3,6 +3,7 @@ from sqlalchemy.exc import IntegrityError
 from project.config import DevelopmentConfig
 from project.dao.models import Genre
 from project.dao.models.director import Director
+from project.dao.models.movie import Movie
 from project.server import create_app
 from project.setup_db import db
 from project.utils import read_json
@@ -17,6 +18,21 @@ with app.app_context():
 
     for director in data["directors"]:
         db.session.add(Director(id=director["pk"],name=director["name"]))
+
+
+    for movie in data["movies"]:
+        db.session.add(
+            Movie(
+                id=movie["pk"],
+                title=movie["title"],
+                description=movie["description"],
+                trailer=movie["trailer"],
+                year=movie["year"],
+                rating=movie["rating"],
+                genre_id=movie["genre_id"],
+                director_id=movie["director_id"],
+            )
+        )
 
     try:
         db.session.commit()
