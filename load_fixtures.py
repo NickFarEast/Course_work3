04751a1edb2d6afba331fe0da63d46f90f6e0109
NickFarEast <shramko.dt@gmail.com>
2022-04-16 19:30:26 +1000

    подготовлены модели , создана база

--- load_fixtures.py
+++ load_fixtures.py
@@ -2,6 +2,7 @@ from sqlalchemy.exc import IntegrityError
 
 from project.config import DevelopmentConfig
 from project.dao.models import Genre
+from project.dao.models.director import Director
 from project.server import create_app
 from project.setup_db import db
 from project.utils import read_json
@@ -14,7 +15,19 @@ with app.app_context():
     for genre in data["genres"]:
         db.session.add(Genre(id=genre["pk"], name=genre["name"]))
 
+    for director in data["directors"]:
+        db.session.add(Director(id=director["pk"],name=director["name"]))
+
     try:
         db.session.commit()
     except IntegrityError:
         print("Fixtures already loaded")
+
+
+    # for director in data["directors"]:
+    #     db.session.add(Director(id=director["pk"],name=director["name"]))
+    #
+    # try:
+    #     db.session.commit()
+    # except IntegrityError:
+    #     print("Fixtures already loaded")
